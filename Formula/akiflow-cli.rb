8b17af23996e7c473593ce82211bedcfc17b45ef
Matt Wilkinson <mattwilki17@gmail.com>
2026-06-02 01:14:23 -0400

    release: v0.3.4

--- Formula/akiflow-cli.rb
+++ Formula/akiflow-cli.rb
@@ -1,7 +1,7 @@
 class AkiflowCli < Formula
   desc "Command-line task management for Akiflow (fork of code-yeongyu/akiflow-cli)"
   homepage "https://github.com/mattwilkinsonn/zireael/tree/main/tools/akiflow-cli"
-  version "0.3.3"
+  version "0.3.4"
   license "MIT"
 
   on_macos do
