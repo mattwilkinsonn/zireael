51b6c3cf0c0147c2e5aba9284cb0f7a3f969507c
Matt Wilkinson <mattwilki17@gmail.com>
2026-06-27 12:39:27 -0400

    fix(ci): keep release formula bumps current

--- Formula/jj-hooks.rb
+++ Formula/jj-hooks.rb
@@ -8,18 +8,18 @@ class JjHooks < Formula
     on_arm do
       url "https://github.com/mattwilkinsonn/zireael/releases/download/v#{version}/jj-hooks-v#{version}-darwin-arm64.tar.gz"
       # SHA256 is bumped by .github/workflows/release.yml when a tag is pushed.
-      sha256 "57c5d9ff776d392868eba2a881c3aea823c69780d6bbfab806183a3df4da4a56"
+      sha256 "5aaf1031cefa51bb7951e4759ee8c09d778fb3ffa0dcd591db4b7a030963e685"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/mattwilkinsonn/zireael/releases/download/v#{version}/jj-hooks-v#{version}-linux-x64.tar.gz"
-      sha256 "e7f5d53dc063aaeefc4e5fb789857116f37455972a25981ae628db7edf2efb9f"
+      sha256 "5311a0b6f7d89a6233cb1c125a001da9c29d492c20e6577e942d6abd1c682694"
     end
     on_arm do
       url "https://github.com/mattwilkinsonn/zireael/releases/download/v#{version}/jj-hooks-v#{version}-linux-arm64.tar.gz"
-      sha256 "498c1c2f5a7ce89b3e11e36ecb7da56b898e10c22e786eb1c0b2d8809f052fbd"
+      sha256 "2ce481a5c2e00e293dc7942b5648c8f9b9790d2388c0e1ee92e6044fdf0e8a16"
     end
   end
 
