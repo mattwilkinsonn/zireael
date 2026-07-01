79518885596a9a014d3db2a13f7f4e891027b518
Matt Wilkinson <mattwilki17@gmail.com>
2026-07-01 15:15:48 -0400

    feat(darwin): Replace Transmit with Mountain Duck

--- nix-config/darwin/system.nix
+++ nix-config/darwin/system.nix
@@ -182,7 +182,7 @@
       "raycast"
       "notion"
       "stablyai/orca/orca"
-      "transmit"
+      "mountain-duck"
     ];
     # Mac App Store apps. IDs from `mas search <name>` / the App Store
     # share-link `/id<NUMBER>`. Requires a one-time manual App Store
